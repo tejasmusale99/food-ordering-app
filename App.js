de0a1423138a2be9f83f6aa58aeaa554c2472106
Tejas Musale <tejasmusale99@gmail.com>
2025-10-13 00:46:53 +0530

    created header component

--- App.js
+++ App.js
@@ -1,8 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import logo from 'url:./SwaadpointLogo.png'
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo-container">
+        <img
+          src={logo}
+          alt="Logo"
+          className="logo"
+        />
+      </div>
+      <nav className="nav">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+        <a href="#" className="cart">
+          🛒 Cart
+        </a>
+      </nav>
+    </header>
+  );
+};
 
 const App = () => {
-    return <h1>Hello</h1>
+    return(
+        <div className="appLayout">
+            <Header />
+        </div>
+    )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
