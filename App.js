f39a1a133cf08097ad923749a38cbca7e346e131
Tejas Musale <tejasmusale99@gmail.com>
2025-10-14 20:57:36 +0530

    created ResataurantCard component

--- App.js
+++ App.js
@@ -20,6 +20,32 @@ const Header = () => {
   );
 };
 
+const RestaurentCard = () => {
+  return (
+    <div
+      className="restaurant-card"
+      // style={{ border: "2px", height: "200px", width: "200px" }}
+    >
+      <div className="card-image">
+        <img
+          src="https://media.istockphoto.com/id/1829241109/photo/enjoying-a-brunch-together.jpg?s=612x612&w=0&k=20&c=9awLLRMBLeiYsrXrkgzkoscVU_3RoVwl_HA-OT-srjQ="
+          alt="The Spice Hub"
+        />
+        <span className="discount"> 20% OFF </span>
+      </div>
+      <div className="card-content">
+        <h3 className="restaurant-name">Maratha Darbar</h3>
+        <p className="cuisine">chinease, indian</p>
+        <div className="card-footer">
+          <span className="rating">⭐ 4.5</span>
+          <span className="delivery-time">20 mins</span>
+          <span className="cost">350 For 2 </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const Body = () => {
   return (
     <div className="body-container">
@@ -31,7 +57,7 @@ const Body = () => {
         <button>Search</button>
       </div>
       <div className="restaurants-container">
-        {/* {Restaurent card} */}
+        <RestaurentCard />
       </div>
     </div>
   );
