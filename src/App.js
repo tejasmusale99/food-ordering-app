8579641707d99032f87adc3e5415435565a905ae
Tejas Musale <tejasmusale99@gmail.com>
2026-02-24 20:29:34 +0530

    Child routing configured

--- src/App.js
+++ src/App.js
@@ -3,29 +3,45 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import About from "./components/About";
+import Contact from "./components/Contact";
+import Cart from "./components/Cart";
 
 const App = () => {
   return (
     <div className="appLayout">
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
 
 const appRouter = createBrowserRouter([
   {
-    path:"/",
+    path: "/",
     element: <App />,
+    children: [
+      {
+        path:"/",
+        element:<Body />
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+    ],
   },
-  {
-    path:"/about",
-    element:<About />
-  }
-])
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<RouterProvider router={appRouter}/>);
+root.render(<RouterProvider router={appRouter} />);
