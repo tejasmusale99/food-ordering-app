8897499f3b6056c96b3e9b32feb27d24d13b08ed
Tejas Musale <tejasmusale99@gmail.com>
2026-02-25 20:11:09 +0530

    updated restaurent menu

--- src/components/RestaurentMenu.js
+++ src/components/RestaurentMenu.js
@@ -13,7 +13,8 @@ const RestaurentMenu = () => {
 
   const fetchResMenu = async () => {
     const menuFetch = await fetch(
-      "https://proxy.corsfix.com/?https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=20.0051498&lng=73.7676967&restaurantId="+resId
+      "https://proxy.corsfix.com/?https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=20.0051498&lng=73.7676967&restaurantId=" +
+        resId,
     );
 
     const menuJson = await menuFetch.json();
@@ -23,9 +24,11 @@ const RestaurentMenu = () => {
     setResMenu(menuJson);
   };
 
-  const info = resMenu?.data?.cards[2]?.card?.card?.info
+  const info = resMenu?.data?.cards[2]?.card?.card?.info;
 
-  console.log(info?.name)
+ 
+
+  console.log(RestaurantItemCategories)
 
   return (
     <div className="bg-gray-100 min-h-screen py-8 px-4 md:px-16">
@@ -36,32 +39,28 @@ const RestaurentMenu = () => {
         <div className="bg-gray-50 rounded-2xl p-5 shadow-inner">
           <div className="flex items-center gap-3 text-sm md:text-base">
             <span className="bg-green-600 text-white px-2 py-1 rounded-full text-xs font-semibold">
-              ★ 4.3
+              ★ {info?.avgRatingString}
             </span>
-            <span className="font-medium text-gray-700">(9.5K+ ratings)</span>
+            <span className="font-medium text-gray-700">
+              {info?.totalRatingsString}
+            </span>
             <span className="text-gray-400">•</span>
-            <span className="font-semibold text-gray-800">₹350 for two</span>
+            <span className="font-semibold text-gray-800">
+              {info?.costForTwoMessage}
+            </span>
           </div>
 
-          <p className="text-orange-600 font-medium mt-3">Pizzas</p>
+          <p className="text-orange-600 font-medium mt-3"> {info?.cuisines + "," }</p>
 
           <div className="mt-3 text-gray-600 text-sm space-y-1">
             <p>
-              <span className="font-semibold">Outlet</span> Ashoka Marg
+              <span className="font-semibold">Outlet</span> {info?.areaName}
             </p>
-            <p>25–30 mins</p>
+            <p>{info?.sla?.slaString}</p>
           </div>
         </div>
       </div>
 
-      {/* Category Component */}
-      {/* {
-        resMenu.map((menuItem)=>{
-          return(
-            <RestaurentMenuCategory />
-          )
-        })
-      } */}
       <RestaurentMenuCategory />
     </div>
   );
