b19e16ebe6e136fd134f22234ec4d04e4c1a46af
Tejas Musale <tejasmusale99@gmail.com>
2026-02-26 21:26:04 +0530

    built a accordian

--- src/components/RestaurentMenuCategory.js
+++ src/components/RestaurentMenuCategory.js
@@ -1,15 +1,21 @@
+import { useState } from "react";
 import ResCategoryItems from "./ResCategoryItems";
 
 const RestaurentMenuCategory = ({ menuCategory }) => {
   // console.log(menuCategory)
 
+  const [isOpen, setIsopen] = useState(true)
+
+  const HandleAccordian = () => {
+    setIsopen(prev => !prev)
+  }
  
   return (
     <div className="max-w-4xl mx-auto mt-10">
       {/* Whole Category Container */}
       <div className="bg-white rounded-2xl shadow-md overflow-hidden">
         {/* Category Header */}
-        <div className="px-6 py-5 flex justify-between items-center cursor-pointer">
+        <div className="px-6 py-5 flex justify-between items-center cursor-pointer" onClick={() => HandleAccordian()}>
           <h2 className="text-xl md:text-2xl font-bold">
             {menuCategory?.card?.card?.title}{" "}
             {"(" + menuCategory?.card?.card?.itemCards?.length + ")"}
@@ -32,7 +38,7 @@ const RestaurentMenuCategory = ({ menuCategory }) => {
 
         <div className="border-t"></div>
 
-        <ResCategoryItems resCategoryItems = {menuCategory}/>
+       {isOpen? <ResCategoryItems resCategoryItems = {menuCategory}/>:""} 
       </div>
     </div>
   );
