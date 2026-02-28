8cac4edb1a346e09019aa1986c943bb5a67ce6f5
Tejas Musale <tejasmusale99@gmail.com>
2026-02-28 22:28:14 +0530

    logic for custom hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,44 +1,20 @@
-import { useEffect, useState } from "react";
+
 // import topRestroChainList from "../utils/topRestroChainList";
 import RestaurentCard from "./RestaurentCard";
 import TopChainCarousel from "./TopChainCarousel";
 import Shimmer from "../components/Shimmer";
 import { Link } from "react-router-dom";
+import useRestaurentCards from "../hooks/useRestaurentCards";
 
 const Body = () => {
-  const [restrolistCards, setRestroListCards] = useState([]);
-  const [searchText, setSearchText] = useState("");
-  const [filtredRestro, setFiltredRestro] = useState([]);
-
-  useEffect(() => {
-    fetchResCard();
-  }, []);
-
-  const fetchResCard = async () => {
-    const fetchRes = await fetch(
-      "https://proxy.corsfix.com/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.9728896&lng=73.8229516&page_type=DESKTOP_WEB_LISTING",
-    );
-    const jsonData = await fetchRes.json();
-    console.log(jsonData);
-
-    const restroCards = jsonData?.data?.cards.find(
-      (item) => item?.card?.card?.gridElements?.infoWithStyle?.restaurants,
-    );
-
-    const restaurants =
-      restroCards?.card?.card?.gridElements?.infoWithStyle?.restaurants || [];
-
-    setRestroListCards(restaurants);
-    setFiltredRestro(restaurants);
-  };
-
-  const searchRestro = (e) => {
-    e.preventDefault();
-    const searchRestroResult = restrolistCards.filter((item) => {
-      return item.info.name.toLowerCase().includes(searchText.toLowerCase());
-    });
-    setFiltredRestro(searchRestroResult);
-  };
+  
+  const {
+  restrolistCards,
+  searchText,
+  setSearchText,
+  filtredRestro,
+  searchRestro
+} = useRestaurentCards();
 
   if (restrolistCards.length === 0) {
     return <Shimmer />;
--- src/hooks/useRestaurentCards.js
+++ src/hooks/useRestaurentCards.js
@@ -1,3 +1,47 @@
+import { useEffect, useState } from "react";
+
 const useRestaurentCards = () => {
-    return 
-}
+  const [restrolistCards, setRestroListCards] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [filtredRestro, setFiltredRestro] = useState([]);
+
+  useEffect(() => {
+    fetchResCard();
+  }, []);
+
+  const fetchResCard = async () => {
+    const fetchRes = await fetch(
+      "https://proxy.corsfix.com/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.9728896&lng=73.8229516&page_type=DESKTOP_WEB_LISTING",
+    );
+    const jsonData = await fetchRes.json();
+    console.log(jsonData);
+
+    const restroCards = jsonData?.data?.cards.find(
+      (item) => item?.card?.card?.gridElements?.infoWithStyle?.restaurants,
+    );
+
+    const restaurants =
+      restroCards?.card?.card?.gridElements?.infoWithStyle?.restaurants || [];
+
+    setRestroListCards(restaurants);
+    setFiltredRestro(restaurants);
+  };
+
+  const searchRestro = (e) => {
+    e.preventDefault();
+    const searchRestroResult = restrolistCards.filter((item) => {
+      return item.info.name.toLowerCase().includes(searchText.toLowerCase());
+    });
+    setFiltredRestro(searchRestroResult);
+  };
+return {
+  restrolistCards,
+  searchText,
+  setSearchText,
+  filtredRestro,
+  searchRestro
+};
+};
+
+
+export default useRestaurentCards;
