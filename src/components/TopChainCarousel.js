e7be71106c3808859f01582c0137725465f2b0a1
Tejas Musale <tejasmusale99@gmail.com>
2026-03-01 12:32:58 +0530

    Link to top chain restro for the restro menu

--- src/components/TopChainCarousel.js
+++ src/components/TopChainCarousel.js
@@ -1,6 +1,7 @@
 import { TopRestroChain } from "./TopRestroChain";
 import CarouselShimmer from "../components/CarouselShimmer";
 import useTopRestroChain from "../hooks/useTopRestroChain";
+import { Link } from "react-router-dom";
 
 const TopChainCarousel = () => {
   const topRestroNashik = useTopRestroChain();
@@ -16,10 +17,12 @@ const TopChainCarousel = () => {
         <div className="top-chain-restaurants-container">
           {topRestroNashik.map((restroChainItem) => {
             return (
+              <Link key={restroChainItem.info.id} to={`/restaurants/${restroChainItem.info.id}`}>
               <TopRestroChain
                 restroChainItem={restroChainItem}
                 key={restroChainItem.info.id}
               />
+              </Link>
             );
           })}
         </div>
