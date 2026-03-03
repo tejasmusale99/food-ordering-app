2d28a594c10e531c79839f36a9ca8247170356c4
Tejas Musale <tejasmusale99@gmail.com>
2026-03-03 21:06:36 +0530

    created cart slice

---
+++ src/utils/features/cartSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+  },
+    reducers: {
+        addItemToCart: (state, action) => {
+            state.items.push(action.payload);
+        },
+        removeItemFromCart: (state, action) => {
+            state.items = state.items.filter(item => item.id !== action.payload);
+        },
+        clearCart: (state) => {
+            state.items = [];
+        }
+    }
+});
+
+export const { addItemToCart, removeItemFromCart, clearCart } = cartSlice.actions;
+export default cartSlice.reducer;
