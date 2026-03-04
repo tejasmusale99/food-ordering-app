46521f574703f2bb71c38ee10e25f14cb03bc775
Tejas Musale <tejasmusale99@gmail.com>
2026-03-04 22:42:59 +0530

    add feature highlights section to About page

--- src/components/About.js
+++ src/components/About.js
@@ -7,11 +7,57 @@ const About = () => {
         </h1>
 
         <p className="text-lg text-gray-600 leading-relaxed">
-          This Swiggy-inspired food ordering application is built using modern 
-          React practices with a strong focus on scalability, performance, and 
-          maintainable architecture. The project demonstrates real-world frontend 
-          engineering concepts such as centralized state management, code splitting, and responsive UI design.
+          This Swiggy-inspired food ordering application is built using modern
+          React practices with a strong focus on scalability, performance, and
+          maintainable architecture. The project demonstrates real-world
+          frontend engineering concepts such as centralized state management,
+          code splitting, and responsive UI design.
         </p>
+      </div>
+
+      <div className="max-w-5xl mx-auto mt-20">
+        <h2 className="text-3xl font-bold text-center mb-12">Key Features</h2>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <div className="bg-white p-6 rounded-2xl shadow hover:shadow-lg transition">
+            <h3 className="text-xl font-semibold mb-3">
+              Dynamic Restaurant Listing
+            </h3>
+            <p className="text-gray-600">
+              Fetches and renders restaurant data dynamically using API
+              integration with proper state management.
+            </p>
+          </div>
+
+          <div className="bg-white p-6 rounded-2xl shadow hover:shadow-lg transition">
+            <h3 className="text-xl font-semibold mb-3">
+              Cart Management with Redux Toolkit
+            </h3>
+            <p className="text-gray-600">
+              Centralized cart state with add, remove,clear cart
+            </p>
+          </div>
+
+          <div className="bg-white p-6 rounded-2xl shadow hover:shadow-lg transition">
+            <h3 className="text-xl font-semibold mb-3">
+              Efficient State Management
+            </h3>
+            <p className="text-gray-600">
+              Managed global cart state using Redux Toolkit with clean reducer
+              logic and derived cart calculations for total price and quantity.
+            </p>
+          </div>
+
+          <div className="bg-white p-6 rounded-2xl shadow hover:shadow-lg transition">
+            <h3 className="text-xl font-semibold mb-3">
+              Code Splitting & Lazy Loading
+            </h3>
+            <p className="text-gray-600">
+              Improved performance by implementing React.lazy and Suspense for
+              on-demand component loading.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
