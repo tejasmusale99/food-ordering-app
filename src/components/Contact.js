3292c3935e502079d0a8d75e6cc10f1a7d431487
Tejas Musale <tejasmusale99@gmail.com>
2026-03-05 23:38:14 +0530

    contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,25 @@
-const Contact = () =>{
-    return(
-        <>Contact</>
-    )
-}
+const Contact = () => {
+  return (
+    <form className="flex-col justify-center align-middle">
+      <div>
+        <label>Your Name</label>
+        <input type="text" className="border-2" />
+      </div>
+      <div>
+        <label>Your Email</label>
+        <input type="email" className="border-2" />
+      </div>
+
+      <div>
+        <label>Contact Number</label>
+        <input type="tel" className="border-2" />
+      </div>
+      <div>
+        <label>Your Message (Optional)</label>
+        <textarea className="border-r-gray-200"></textarea>
+      </div>
+    </form>
+  );
+};
 
 export default Contact;
